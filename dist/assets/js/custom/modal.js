0faca2a9cb424755b5ca41a7b36b06d959afe004
Dodik Vexel <sabdoilahidodik735@gmail.com>
2025-05-21 23:40:20 +0700

    fix modal

---
+++ dist/assets/js/custom/modal.js
@@ -0,0 +1,27 @@
+/******/ (() => { // webpackBootstrap
+/*!***************************************!*\
+  !*** ./src/assets/js/custom/modal.js ***!
+  \***************************************/
+function openModal() {
+  const modal = document.getElementById("modal");
+  modal.style.display = "flex";
+}
+
+function closeModal() {
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  const modal = document.getElementById("modal");
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
+
+
+window.openModal = openModal;
+window.closeModal = closeModal;
+/******/ })()
+;
+//# sourceMappingURL=modal.js.map
---
+++ src/assets/js/custom/modal.js
@@ -0,0 +1,20 @@
+function openModal() {
+  const modal = document.getElementById("modal");
+  modal.style.display = "flex";
+}
+
+function closeModal() {
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  const modal = document.getElementById("modal");
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
+
+
+window.openModal = openModal;
+window.closeModal = closeModal;
